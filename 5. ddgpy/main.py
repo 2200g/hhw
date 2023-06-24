f6dd06a21c9293ce91620d9784c460729664c597
aadi <aadi@aadi.net.in>
2023-06-24 12:26:40 +0530

    i use arch btw.

--- 5. ddgpy/main.py
+++ 5. ddgpy/main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def search_duckduckgo(query):
     response = requests.get(f"https://duckduckgo.com/html/?q={query}",
-                            headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"})
+                            headers={"User-Agent": "Mozilla/5.0 (X11; Arch Linux; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"})
     
     if response.ok:
         results = BeautifulSoup(response.content, "html.parser").select(".result.results_links.results_links_deep.web-result")
